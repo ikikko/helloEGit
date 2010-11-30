fcf85733784418600aa6f67908531a4b1cbaabb3
ikikko <ikikko+github@gmail.com>
2010-12-01 01:33:26 +0900

    ハローワールド追加

--- src/main/java/com/github/ikikko/helloEGit/App.java
+++ src/main/java/com/github/ikikko/helloEGit/App.java
@@ -2,12 +2,11 @@ package com.github.ikikko.helloEGit;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello, EGit");
+	}
 }
