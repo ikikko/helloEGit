e3bff74ff4498613baa4d963e6489509e858ecf9
ikikko <ikikko+github@gmail.com>
2010-12-01 01:31:52 +0900

    EGitの検証用コミット

---
+++ src/main/java/com/github/ikikko/helloEGit/App.java
@@ -0,0 +1,13 @@
+package com.github.ikikko.helloEGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
